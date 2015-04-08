7fbc3f2f03c3cb9e385c21ce4bc0de3ed51a4f30
nraov <nageswara.raov@gmail.com>
2015-04-08 21:58:28 +0530

    - Updated log4.j - Updated serive path in run.sh

--- src/main/scripts/run.sh
+++ src/main/scripts/run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #-------------------------------------------------------------
-# running fpservice
+# running service
 # author: Nageswara Rao
 # since: Feb 17 , 2015.
 #-------------------------------------------------------------
 
 echo "setting environment for FPService"
 
-FPSERVICE_HOME=`pwd`
-export FPSERVICE_HOME
-echo "FPService home is set to" $FPSERVICE_HOME
-cd $FPSERVICE_HOME
+SERVICE_HOME=`pwd`
+export SERVICE_HOME
+echo "SERVICE_HOME is set to" $SERVICE_HOME
+cd $SERVICE_HOME
 
 java -jar -Xmx1024m -Xms512m ${project.artifactId}-${project.version}.jar -D config=resources/config.json
